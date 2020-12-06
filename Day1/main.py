00df2fb72007ba60ca440a86c9294a79ac0f4d82
fahrenq <ivankuznetsov316@gmail.com>
2020-12-06 08:55:10 +0100

    Day 1 part 2

--- Day1/main.py
+++ Day1/main.py
@@ -1,13 +1,16 @@
 # https://adventofcode.com/2020/day/1
-def filter_candidates(last_digit):
-    def f(x):
-        l = int(x[-1])
-        if last_digit == 0:
-            return l == 0
-        else:
-            return l == (10 - last_digit)
 
-    return f
+# def filter_candidates(last_digit):
+#     def f(x):
+#         l = int(x[-1])
+#         if last_digit == 0:
+#             return l == 0
+#         else:
+#             return l == (10 - last_digit)
+#
+#     return f
+# last_digit = int(i[-1])
+# candidates = list(filter(filter_candidates(last_digit), lines[idx + 1:]))
 
 
 def main():
@@ -15,15 +18,18 @@ def main():
         lines = f.read().split('\n')
 
     for idx, i in enumerate(lines):
-        last_digit = int(i[-1])
-        candidates = list(filter(filter_candidates(last_digit), lines[idx + 1:]))
         i_int = int(i)
-        for c in candidates:
-            c_int = int(c)
-            if i_int + c_int == 2020:
-                print(f'Solution: {i} and {c}')
-                print(f'{i} * {c} = {i_int * c_int}')
-                break
+        for s_idx, s in enumerate(lines[idx + 1:]):
+            s_int = int(s)
+            if i_int + s_int == 2020:
+                print(f'Solution for 2 numbers found: {i} and {s}')
+                print(f'{i} * {s} = {i_int * s_int}\n')
+            elif i_int + s_int < 2020:
+                for t in lines[s_idx + 1:]:
+                    t_int = int(t)
+                    if i_int + s_int + t_int == 2020:
+                        print(f'Solution for 3 numbers found: {i} and {s} and {t}')
+                        print(f'{i} * {s} * {t} = {i_int * s_int * t_int}\n')
 
 
 if __name__ == '__main__':
